2ac765ca988e77986cfdd79c4cb2d7daaa43c239
pdpAddl <patrick.schaible1@gmail.com>
2022-05-25 13:51:12 +0200

    secret storage of keys in a local .env file

--- Binance_Bot.py
+++ Binance_Bot.py
@@ -1,16 +1,49 @@
 import os
 from binance.client import Client
+from time import sleep
+from dotenv import dotenv_values, load_dotenv
 
-# init
-api_key = os.environ.get('binance_api')
-api_secret = os.environ.get('binance_secret')
+from binance import ThreadedWebsocketManager
 
-client = Client("kXGSgmxSbYqY5G1OgeU1ypaZ2yebh8FXH9i73aLttMATci9YHy48FPNi1VghVBYt", "xW79WjCMIZqxeKP13z9yZRUSAxViqbejlclbNVdk3wSSZprbK8Uv4jbR9j4DhVG8")
+# get API Keys
+config=dotenv_values("local.env")
 
-print(client)
+api_key = config["binance_api"]
+api_secret = config["binance_secret"]
+
+print(api_key, api_secret)
+
+client = Client(api_key, api_secret)
 
 # Demo Account
 client.API_URL = 'https://testnet.binance.vision/api'
 
-# get balances for all assets & some account information
+
 print(client.get_asset_balance(asset='BTC'))
+
+# Init bitcoin price variable
+btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
+btc_price = {'error':False}
+
+print(btc_price)
+
+'''
+def btc_trade_history(msg):
+    #define how to process incoming WebSocket messages 
+    if(msg['e'] != 'error'):
+        print(msg['c'])
+        btc_price['last'] = msg['c']
+        btc_price['bid'] = msg['b']
+        btc_price['last'] = msg['a']
+        btc_price['error'] = False
+    else:
+        btc_price['error'] = True
+
+async def stream():
+    # init and start the WebSocket
+    bsm = ThreadedWebsocketManager()
+    bsm.start()
+
+    # subscribe to a stream
+    bsm.start_symbol_ticker_socket(callback=btc_trade_history, symbol='BTCUSDT')
+    '''
