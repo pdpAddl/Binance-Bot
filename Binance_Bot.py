985d548be60ba3a6a91764332ba1bb6780859f92
pdpAddl <patrick.schaible1@gmail.com>
2022-05-25 12:20:30 +0200

    New venv with correct packages

--- Binance_Bot.py
+++ Binance_Bot.py
@@ -5,10 +5,12 @@ from binance.client import Client
 api_key = os.environ.get('binance_api')
 api_secret = os.environ.get('binance_secret')
 
-client = Client(api_key, api_secret)
+client = Client("kXGSgmxSbYqY5G1OgeU1ypaZ2yebh8FXH9i73aLttMATci9YHy48FPNi1VghVBYt", "xW79WjCMIZqxeKP13z9yZRUSAxViqbejlclbNVdk3wSSZprbK8Uv4jbR9j4DhVG8")
+
+print(client)
 
 # Demo Account
 client.API_URL = 'https://testnet.binance.vision/api'
 
 # get balances for all assets & some account information
-print(client.get_account())
+print(client.get_asset_balance(asset='BTC'))
